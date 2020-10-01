0f563505c81c80d135d662bccc893f034de9065a
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-01 17:16:01 +0530

    jquery

--- assets/js/jquery.form.js
+++ assets/js/jquery.form.js
@@ -993,7 +993,7 @@ $.fn.fieldSerialize = function(successful) {
  *  v == ['C1']
  *
  * The successful argument controls whether or not the field element must be 'successful'
- * (per http://www.w3.org/TR/html4/interact/forms.html#successful-controls).
+ * (per http://www.w3.org/TR/html4/interact/forms.jsp#successful-controls).
  * The default value of the successful argument is true.  If this value is false the value(s)
  * for each element is returned.
  *
