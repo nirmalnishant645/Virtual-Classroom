4ae4168274b7e33290b5c09a17d57f92deb2c244
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-15 21:19:07 +0530

    DB

--- project_DB.sql
+++ project_DB.sql
@@ -17,6 +17,8 @@ INSERT INTO enroll_for (degree, course) values ('M.Sc', 'Computer Science');
 INSERT INTO enroll_for (degree, course) values ('M.Sc', 'Computer Application');
 INSERT INTO enroll_for (degree, course) values ('B.A.', 'English');
 INSERT INTO enroll_for (degree, course) values ('B.A.', 'Psychology');
+INSERT INTO enroll_for (degree, course) values ('M.A.', 'English');
+INSERT INTO enroll_for (degree, course) values ('M.A.', 'Psychology');
 
 CREATE TABLE student ( roll_no integer PRIMARY KEY, name varchar(50) NOT NULL, password BLOB NOT NULL, email_id varchar(255) NOT NULL, degree varchar(5) NOT NULL, course varchar(30) NOT NULL, year integer NOT NULL, is_approved BOOLEAN DEFAULT false,  CONSTRAINT CHK_year CHECK (year BETWEEN 1 AND 3)) ENGINE = InnoDB;
 
